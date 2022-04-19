e66a3bd96a60f3437c96d55596bb9120d587e8fe
Ethan Schneider <61173518+etnsch@users.noreply.github.com>
2022-04-19 19:00:08 -0400

    Update resume.js
    testing link

--- pages/posts/resume.js
+++ pages/posts/resume.js
@@ -1,3 +1,3 @@
 export default function resume() {
-    return <iframe src="https://drive.google.com/file/d/1NycR265qKr_L99GmCRFJtLqMZ36A60Tf/view" width="1280" height="960"></iframe>
+    return <iframe src="https://drive.google.com/file/d/1NycR265qKr_L99GmCRFJtLqMZ36A60Tf/preview" width="1280" height="960"></iframe>
   }
